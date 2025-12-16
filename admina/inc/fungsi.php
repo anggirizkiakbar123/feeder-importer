8cbe7701ba2b86469e605d883b70d9da4936fe9d
Anggirizkiakbar272 <157447360+Anggirizkiakbar272@users.noreply.github.com>
2025-12-16 09:36:51 +0700

    Update fungsi.php

--- admina/inc/fungsi.php
+++ admina/inc/fungsi.php
@@ -1,78 +1,110 @@
-<?php 
-//url parsing
-function parse_path() {
-  $path = array();
-  if (isset($_SERVER['REQUEST_URI'])) {
-    $request_path = explode('?', $_SERVER['REQUEST_URI']);
+<?php
+session_start();
 
-    $path['base'] = rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/');
-    $path['call_utf8'] = substr(urldecode($request_path[0]), strlen($path['base']) + 1);
-    $path['call'] = utf8_decode($path['call_utf8']);
-    if ($path['call'] == basename($_SERVER['PHP_SELF'])) {
-      $path['call'] = '';
-    }
-    $path['call_parts'] = explode('/', $path['call']);
-
-
-    if ($request_path[1]='') {
-      $path['query_utf8'] = urldecode($request_path[1]);
-       $path['query'] = utf8_decode(urldecode($request_path[1]));
-    $vars = explode('&', $path['query']);
-    foreach ($vars as $var) {
-      $t = explode('=', $var);
-      $path['query_vars'][$t[0]] = $t[1];
-    }
-    }
-  
-  }
-return $path;
-}
-
-//first url path
-$path = parse_path();
-$path_url = $path['call_parts'][1];
-
-
-//second path
-$path_act = (isset($path['call_parts'][2]))?$path['call_parts'][2]:'';
-//third path
-$path_id = (isset($path['call_parts'][3]))?$path['call_parts'][3]:'';
-
-//fourth path
-$path_four = (isset($path['call_parts'][4]))?$path['call_parts'][4]:'';
-
-
-//fourth five
-$path_five = (isset($path['call_parts'][5]))?$path['call_parts'][5]:'';
-
-
-//simpan role url page user di array sesuai login session level
-  $role_user=array();
-  $role_act=array();
-foreach ($db->query("select sys_menu.url from sys_menu inner join sys_menu_role on sys_menu.id=sys_menu_role.id_menu
-    where sys_menu_role.group_id=? and sys_menu_role.read_act=?",array('sys_menu_role.group_id'=>$_SESSION['level'],'sys_menu_role.read_act'=>'Y')) as $role) {
-  $role_user[]=$role->url;
-}
-//lebih detail detil crud role user
-foreach ($db->query("select read_act,insert_act,update_act,delete_act from sys_menu inner join sys_menu_role on sys_menu.id=sys_menu_role.id_menu where sys_menu_role.group_id=? and sys_menu.url=?",array('sys_menu_role.group_id'=>$_SESSION['level'],'sys_menu.url'=>$path_url)) as $role) {
-  $role_act['up_act']=$role->update_act;
-  $role_act['insert_act']=$role->insert_act;
-  $role_act['del_act']=$role->delete_act;
-}
-
-//obj to array
-function toArray($obj)
+function parse_path(): array
 {
-    if (is_object($obj)) $obj = (array)$obj;
-    if (is_array($obj)) {
-        $new = array();
-        foreach ($obj as $key => $val) {
-            $new[$key] = toArray($val);
-        }
-    } else {
-        $new = $obj;
+    $path = [
+        'base'       => '',
+        'call'       => '',
+        'call_parts' => [],
+        'query'      => '',
+        'query_vars' => []
+    ];
+
+    if (!isset($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME'])) {
+        return $path;
     }
 
-    return $new;
+    $request_path = explode('?', $_SERVER['REQUEST_URI'], 2);
+
+    $path['base'] = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+
+    $call = urldecode($request_path[0]);
+    $call = ltrim(substr($call, strlen($path['base'])), '/');
+
+    if ($call === basename($_SERVER['PHP_SELF'])) {
+        $call = '';
+    }
+
+    $path['call'] = $call;
+    $path['call_parts'] = $call !== '' ? explode('/', $call) : [];
+
+    if (isset($request_path[1]) && $request_path[1] !== '') {
+        $path['query'] = urldecode($request_path[1]);
+        parse_str($path['query'], $path['query_vars']);
+    }
+
+    return $path;
 }
-?>
+
+$path = parse_path();
+
+$path_url  = $path['call_parts'][1] ?? '';
+$path_act  = $path['call_parts'][2] ?? '';
+$path_id   = $path['call_parts'][3] ?? '';
+$path_four = $path['call_parts'][4] ?? '';
+$path_five = $path['call_parts'][5] ?? '';
+
+/* =========================
+   SESSION CHECK
+========================= */
+if (!isset($_SESSION['level'])) {
+    die('Unauthorized access');
+}
+
+/* =========================
+   ROLE & PERMISSION
+========================= */
+$role_user = [];
+$role_act  = [];
+
+/* READ MENU ROLE */
+$stmt = $db->prepare("
+    SELECT sys_menu.url
+    FROM sys_menu
+    INNER JOIN sys_menu_role 
+        ON sys_menu.id = sys_menu_role.id_menu
+    WHERE sys_menu_role.group_id = ?
+      AND sys_menu_role.read_act = ?
+");
+$stmt->execute([$_SESSION['level'], 'Y']);
+
+while ($role = $stmt->fetch(PDO::FETCH_OBJ)) {
+    $role_user[] = $role->url;
+}
+
+/* CRUD PERMISSION */
+$stmt = $db->prepare("
+    SELECT read_act, insert_act, update_act, delete_act
+    FROM sys_menu
+    INNER JOIN sys_menu_role 
+        ON sys_menu.id = sys_menu_role.id_menu
+    WHERE sys_menu_role.group_id = ?
+      AND sys_menu.url = ?
+");
+$stmt->execute([$_SESSION['level'], $path_url]);
+
+if ($role = $stmt->fetch(PDO::FETCH_OBJ)) {
+    $role_act = [
+        'read_act'   => $role->read_act,
+        'insert_act' => $role->insert_act,
+        'up_act'     => $role->update_act,
+        'del_act'    => $role->delete_act
+    ];
+}
+
+/* =========================
+   OBJECT TO ARRAY (SAFE)
+========================= */
+function toArray($data)
+{
+    if (is_object($data)) {
+        $data = (array) $data;
+    }
+
+    if (is_array($data)) {
+        return array_map('toArray', $data);
+    }
+
+    return $data;
+}
